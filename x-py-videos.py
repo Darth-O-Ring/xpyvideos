b4fcfb1e8464907d57afaf9add04986ecdc41fe4
Darth-O-Ring <darthoring@gmail.com>
2013-10-14 09:57:27 -0700

    moved regexes into one function

--- x-py-videos.py
+++ x-py-videos.py
@@ -30,7 +30,7 @@ import os
 from getpass import getuser
 
 # Version number
-VERSION		=		'v0.0.2'
+VERSION		=		'v0.0.2-1'
 
 
 def build_opts():
@@ -97,7 +97,8 @@ def file_name(arg_f, URL):
 		return arg_f
 
 
-def regex_xvideos(HTML):
+def regex_for_video_link(HTML):
+	
 	"""
 	Uses regular expression to search for flv file link.
 
@@ -105,59 +106,43 @@ def regex_xvideos(HTML):
 
 	"""
 
-	# Use regular expression to search html for flv url
+	# Use regular expression to search html for video's url
 	reg_ex_for_link		=		re.compile(r'''
 									# Don't match beginning of html
-							(flv_url=)	# Start matching once ('flv_url=') is found
+							flv_url=	# Start matching once 'flv_url=' is found
 							
-							(\S+)		# Match one or more non-whitespace characters 
-									# after ('flv_url=')
+							(\S+?)		# Group one or more non-whitespace characters 
+									# in a non-greedy way so only one link is found
 									# This is the video link needed for download
 							
-							(amp)		# Match ('url_').  This marks the end of the video link
-
-								''', re.VERBOSE)
-
-	# Grab video link from three item tuple given by .groups()
-	# Handle valid URLs but invalid Xvideo URL errors, causing regex to return a None type, while searching html
-	try:
-		return reg_ex_for_link.search(HTML).groups()[1]
-
-	# Catch regex exception
-	except (AttributeError, IndexError):
-		print "\n\nError: None type returned.\nCheck that URL is valid: ('http://www.xvideos.com/remaining_link')\n\n"
-		sys.exit(2)
-
-
-def regex_xhamster(HTML):
-	"""
-	Uses regular expression to search for mp4 file.
-
-	HTML	:	html returned by .read()
-
-	"""
-
-	# Use regular expression to search html for mp4
-	reg_ex_for_link		=		re.compile(r'''
-									# Don't match beginning of html
-							(mp4File=)	# Start matching once ('mp4File=') is found
+							amp		# Stop matching here.  This marks the end of the video link
 							
-							(\S+)		# Match one or more non-whitespace characters 
-									# after ('mp4File=')
-									# This is the video file needed for download
-							(")		# Marks end of mp4 file
+							|		# Mutually exclusive pattern.  Match above or below.
 
-								''', re.VERBOSE)
+							mp4File=	# Start matching at 'mp4File='
 
-	# Grab video link from three item tuple given by .groups()
-	# Handle valid URLs but invalid Xhamster URL errors, causing regex to return a None type, while searching html
+							(\S+?)		# Group one or more non-whitespace characters in a
+									# non-greedy way so only one link is found
+									# This is the video link needed for download.
+
+							"		# Stop matching here.  This marks the end of the video link
+
+							''', re.VERBOSE)
+
+	# Return video link from two item tuple returned by .groups()
+	# Handle valid URLs but invalid Xvideo/Xhamster URL errors, causing regex to return a None type, while searching html
 	try:
-		return reg_ex_for_link.search(HTML).groups()[1]
+
+	# Check for where in the tuple the link is
+			if reg_ex_for_link.search(HTML).groups()[0] is None:
+				return reg_ex_for_link.search(HTML).groups()[1]
+			else:
+				return reg_ex_for_link.search(HTML).groups()[0]
 
 	# Catch regex exception
 	except (AttributeError, IndexError):
-		print """\n\nError: None type returned.\nCheck that URL is valid: ('http://www.xvideos/ORxhamster.com/remaining_link')
-				\n\n"""
+		print """\n\nError: None type returned.\n
+				Check that URL is valid: ('http://www.xvideos_OR_xhamster.com/remaining_link')\n\n"""
 		sys.exit(2)
 
 
@@ -185,7 +170,7 @@ def write_video_to_file(arg_dir, vid_file, f_name):
 	# Handle file opening/writing errors
 	try:
 		with open(pathname, 'wb') as output:
-			print "\nDownloading: {0} (Bytes: {1})\n\n".format(f_name, file_size)
+			print "\nDownloading: '{0}' (Bytes: {1})\n\n".format(f_name, file_size)
 			
 
 	# Start loop to display progress bar
@@ -244,23 +229,25 @@ def download_video(args):
 
 	# Catch URL exceptions
 	except (urllib2.URLError, urllib2.HTTPError):
-		print """\n\nError: Check that URL is valid: ('http://www.xvideos/OR_xhamster.com/remaining_link')
+		print """\n\nError: Check that URL is valid: ('http://www.xvideos_OR_xhamster.com/remaining_link')
 				\nFailed on: ('{}')\n\n""".format(url)
 		sys.exit(2)
+	
+	# Use regex to search html for the video link
+	video_file		=		regex_for_video_link(html)
+	
 	
 	# Call file_name and assign return value to filename variable
 	filename		=		file_name(args['f'], url)
 	
 	# Check for xvideos 
 	if 'xvideos.com' in url:
-		video_file		=		regex_xvideos(html)
 	
 	# Add .flv to filename
 		filename		=		''.join((filename, '.flv'))
 
 	# Check for xhamster
 	elif 'xhamster.com' in url:
-		video_file		=		regex_xhamster(html)
 
 	# Replace .html with .mp4 in the case of xhamster
 		filename		=		filename.replace('.html', '.mp4')
