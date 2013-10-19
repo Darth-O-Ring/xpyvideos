376eab8a4cded43176c8d864eab88c74eda180be
Darth-O-Ring <darthoring@gmail.com>
2013-10-18 22:36:19 -0700

    install

---
+++ setup.py
@@ -0,0 +1,55 @@
+import sys, os, shutil
+from distutils.core import setup, Extension
+
+shutil.copyfile("xpyvideos.py", "X-Py-Videos/xpyvideos.py")
+
+setup(name		=		'x-py-videos',
+      version		=		'0.0.2-4',
+      description	=		'Python program for downloading videos from xvideos, xhamster, and redtube.',
+      author		=		'Darth_O-Ring',
+      author_email	=		'darthoring@gmail.com',
+      url		=		'https://github.com/Darth-O-Ring/X-Py-Videos',
+      packages		=		['xpyvideos'],
+      package_dir	=		{'xpyvideos' : 'X-Py-Videos/'},
+      scripts		=		['X-Py-Videos/xpyvideos.py'],
+      data_files	=		[('share/xpyvideos', ['README.md', 'NOTICE'])]
+	)
+
+
+
+print "Cleaning up..."
+try:
+	removeall("build/")
+    	os.rmdir("build/")
+except:
+	pass
+
+try:
+	os.remove("x-py-videos/xpyvideos")
+except:
+	 pass
+
+def capture(cmd):
+	return os.popen(cmd).read().strip()
+
+def removeall(path):
+	if not os.path.isdir(path):
+		return
+
+	files=os.listdir(path)
+
+	for x in files:
+		fullpath=os.path.join(path, x)
+		if os.path.isfile(fullpath):
+			f=os.remove		
+			rmgeneric(fullpath, f)
+		elif os.path.isdir(fullpath):
+			removeall(fullpath)
+			f=os.rmdir
+			rmgeneric(fullpath, f)
+																
+def rmgeneric(path, __func__):
+	try:
+		__func__(path)													
+	except OSError, (errno, strerror):
+		pass
