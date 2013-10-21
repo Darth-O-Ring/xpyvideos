18f03552f535c5da243aaf30c2d15e0c15d713f7
Darth-O-Ring <darthoring@gmail.com>
2013-10-21 12:01:15 -0700

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python2
 
 import xpyvideos
-import shutil
 from distutils.core import setup
 from setuptools import find_packages
 
-shutil.copyfile("xpyvideos.py", "xpyvideos/xpyvideos")
 
 setup(name		=		'xpyvideos',
       version		=		'0.0.2-4',
@@ -14,8 +12,8 @@ setup(name		=		'xpyvideos',
       author_email	=		'darthoring@gmail.com',
       url		=		'https://github.com/Darth-O-Ring/xpyvideos',
       packages		=		find_packages(),
-      package_dir	=		{'xpyvideos' : 'xpyvideos'},
-      scripts		=		['xpyvideos.py'],
+      package_dir	=		{'xpyvideos' : 'xpyvideos/'},
+      scripts		=		['xpyvideos/xpyvideos.py'],
       data_files	=		[('share/xpyvideos', ['README.md', 'NOTICE'])]
 	)
 
