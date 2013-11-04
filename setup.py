ad4696b28b864d9f9fc17a909fc51adfc37da8a3
Darth-O-Ring <darthoring@gmail.com>
2013-11-04 00:03:27 -0800

    Added removing of opts.py.

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ import xpyvideos
 from distutils.core import setup
 from setuptools import find_packages
 from os import remove
+from os.path import abspath
+from os.path import join as path_join
+from os import getcwd
 
+pathname		=		getcwd()
 
 setup(name		=		'xpyvideos',
       version		=		'0.0.2-7',
@@ -18,8 +22,19 @@ setup(name		=		'xpyvideos',
       						'xpyvideos/filename.py'],
       data_files	=		[('share/xpyvideos', ['README.md', 'NOTICE'])]
 	)
+
+
 try:
-	remove('/usr/bin/opts.py')
-	print '\n\nCleaning up...\n\n'
+	remove(abspath('/usr/bin/opts.py'))
+except:
+	pass
+
+try:
+	remove(abspath(path_join(pathname, 'build/scripts-2.7/opts.py')))
+except:
+	pass
+
+try:
+	remove(abspath(path_join(pathname, 'build/lib/xpyvideos/opts.py')))
 except:
 	pass
