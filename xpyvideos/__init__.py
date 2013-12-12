01bc9a5620ae01cf5dfdcf8b7302005ed6288f99
Darth-O-Ring <darthoring@gmail.com>
2013-12-12 15:33:53 -0800

    Added lines in init file.

--- xpyvideos/__init__.py
+++ xpyvideos/__init__.py
@@ -1,1 +1,6 @@
-# Blank
+__title__       =       'xpyvideos'
+__version__     =       '0.0.2-9'
+__author__      =       'Darth_O-Ring'
+__license__     =       'GPL'
+__copyright__   =       'Copyright 2013-2015 Darth_O-Ring'
+ 
