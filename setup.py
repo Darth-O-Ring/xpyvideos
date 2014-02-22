788e37c5652c80c6805fc7aa725f6f282eec5cbe
Darth-O-Ring <darthoring@gmail.com>
2014-02-21 23:26:28 -0800

    Updated version number and added glob generator to remove previous egg-infos.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from os.path import join as path_join
 from os import getcwd
 from shutil import copyfile, rmtree
 from os import remove
+import glob
 
 import xpyvideos
 
@@ -18,7 +19,7 @@ copyfile("xpyvideos.py", "xpyvideos/xpyvideos")
 packages        =       ['xpyvideos', 'xpyvideos.packages', 'xpyvideos.packages.pytube']
 
 setup(name		    =		'xpyvideos',
-      version		=		'0.0.2-9',
+      version		=		'0.0.3-0',
       description	=		'Python program for downloading videos from xvideos, xhamster, and redtube.',
       author		=		'Darth_O-Ring',
       author_email	=		'darthoring@gmail.com',
@@ -32,6 +33,7 @@ setup(name		    =		'xpyvideos',
 
 files		=		['/usr/bin/opts.py', '/usr/bin/filename.py', '/usr/bin/download.py', '/usr/bin/regexes.py',
 					'/usr/bin/xpyvideos.py', 'xpyvideos/xpyvideos']
+prev_eggs   =       glob.iglob('/usr/lib/python2.7/site-packages/xpyvideos-0.0.2*')
 
 for i in xrange(len(files)):
 	try:
@@ -41,6 +43,11 @@ for i in xrange(len(files)):
 			remove(abspath(path_join(pathname, files[i])))
 	except:
 		pass
+for i in prev_eggs:
+    try:
+        remove(i)
+    except:
+        pass
 
 try:
 	rmtree(abspath(path_join(pathname, 'build/')))
