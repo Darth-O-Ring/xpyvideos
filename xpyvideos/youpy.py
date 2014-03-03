2028766727fabd701d00066505103ac5a31c745c
Darth-O-Ring <darthoring@gmail.com>
2014-03-03 12:01:07 -0800

    Updated parameters and functions to reflect multiple link/filename changes.

--- xpyvideos/youpy.py
+++ xpyvideos/youpy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 
 # Imports
+from __future__ import print_function
 import subprocess
 import re
 from sys import exit
@@ -16,7 +17,7 @@ from .packages.pytube.exceptions import MultipleObjectsReturned, YouTubeError
 __Author__ = "bmcg"
 __Email__ = "bmcg0890@gmail.com"
 __License__ = """
-Copyright (C) 2013-2015  bmcg	<bmcg0890@gmail.com>
+Copyright (C) 2013-2015  bmcg   <bmcg0890@gmail.com>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -34,185 +35,186 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 def menu(lst):
-	"""
-	Prints out available video types 
-	in a menu form
+    """
+    Prints out available video types 
+    in a menu form
 
-	lst		:	list of strings to be printed out as a menu
+    lst     :   list of strings to be printed out as a menu
 
-	"""
-	# Assign an empty list for mapping user input to list item
-	item_count	=		1
+    """
+    # Assign an empty list for mapping user input to list item
+    item_count  =       1
 
-	# Print out items in original list to screen
-	for i in xrange(len(lst)):
-		print '{0:>2}) {1:<30}'.format(item_count, lst[i])
+    # Print out items in original list to screen
+    for i in xrange(len(lst)):
+        print('{0:>2}) {1:<30}'.format(item_count, lst[i]))
 
-	# After each item in the list is printed, append it with the item_count number to new_lst
-		lst[i]			=		lst[i].replace(lst[i], ''.join((str(item_count), lst[i])))
-		item_count 		+=		1
+    # After each item in the list is printed, append it with the item_count number to new_lst
+        lst[i]          =       lst[i].replace(lst[i], ''.join((str(item_count), lst[i])))
+        item_count      +=      1
 
-	# Loop until the user's input corresponds to an item in new_lst
-	while True:
-		try:
-			user_in			=		raw_input('\n> ')
+    # Loop until the user's input corresponds to an item in new_lst
+    while True:
+        try:
+            user_in         =       raw_input('\n> ')
 
-	# Loop the items in new_lst and check the beginning for matching user input to item in new_lst 
-			for i in xrange(len(lst)):
-				if user_in in lst[i][:len(user_in)]:
-					return lst[int(user_in) - 1][len(user_in):]
+    # Loop the items in new_lst and check the beginning for matching user input to item in new_lst 
+            for i in xrange(len(lst)):
+                if user_in in lst[i][:len(user_in)]:
+                    return lst[int(user_in) - 1][len(user_in):]
 
-	# If the user's input doesn't correspond to an item in new_lst print an invalid selection message
-			else:
-				print '\nInvalid selection.  Try again.\n'
+    # If the user's input doesn't correspond to an item in new_lst print an invalid selection message
+            else:
+                print('\nInvalid selection.  Try again.\n')
 
-	# Break and quit program if keyboard interrupt signal is given
-		except KeyboardInterrupt:
-			print '\n\nInterrrupt signal given.\nQuitting...\n\n'
-			exit(1)
+    # Break and quit program if keyboard interrupt signal is given
+        except KeyboardInterrupt:
+            print('\n\nInterrrupt signal given.\nQuitting...\n\n')
+            exit(1)
 
 
-def download_youtube(args):
-	"""
-	Downloads youtube video
+def download_youtube(args, url, f_name):
+    """
+    Downloads youtube video
 
-	args		:	parsed command line arguments
+    args        :   parsed command line arguments
+    url         :   url of video grabbed from main() for loop
 
-	"""
+    """
 
-	# Call the YoutTube Function
-	youtube			=	YouTube()
+    # Call the YoutTube Function
+    youtube         =       YouTube()
 
-	# Set the video url
-	try:
-		youtube.url		=	args['u']
+    # Set the video url
+    try:
+        youtube.url     =       url
 
-	# Catch invalid YouTube URLs
-	except YouTubeError:
-		print "\n\nError:  Failed on ('{}').\nCheck for valid YouTube URL.\n\n".format(args['u'])
-		exit(2)
+    # Catch invalid YouTube URLs
+    except YouTubeError:
+        print("\n\nError:  Failed on ('{}').\nCheck for valid YouTube URL.\n\n".format(url))
+        exit(2)
 
-	# Create menu of video format/resolution options
-	video_option_selection	=	menu(map(str, youtube.videos))
+    # Create menu of video format/resolution options
+    video_option_selection  =   menu(map(str, youtube.videos))
 
-	# Extract video types into a list of a single string
-	video_format 		=	re.findall(r'\(.(\w{3,4})\)', video_option_selection)[0]
-	video_resolution	=	re.findall(r'-\s*(\w{3,4})', video_option_selection)[0]
+    # Extract video types into a list of a single string
+    video_format        =   re.findall(r'\(.(\w{3,4})\)', video_option_selection)[0]
+    video_resolution    =   re.findall(r'-\s*(\w{3,4})', video_option_selection)[0]
 
-	# Set filename if -f/--filename option is given
-	if args['f']:
-		youtube.filename		=		args['f']
+    # Set filename if -f/--filename option is given
+    if f_name is not None:
+        youtube.filename        =       f_name
 
-	# Set the video format	
-	try:
-		set_video_format_res	=	youtube.get(video_format, video_resolution)
+    # Set the video format  
+    try:
+        set_video_format_res    =   youtube.get(video_format, video_resolution)
 
-	# Catch multiple videos returned error
-	except MultipleObjectsReturned:
-		print '\n\nError: More than one video returned.\n\n'
-		exit(1)
+    # Catch multiple videos returned error
+    except MultipleObjectsReturned:
+        print('\n\nError: More than one video returned.\n\n')
+        exit(1)
 
-	# Download video
-	set_video_format_res.download(args['dir'],on_progress=print_status, on_finish=video_to_mp3 if args['c'] else None)
+    # Download video
+    set_video_format_res.download(args['dir'],on_progress=print_status, on_finish=video_to_mp3 if args['c'] else None)
 
-	# Delete original video file if -do/--delete and -c/--convert is given
-	if args['c'] and args['do']:
-	
-	# Call remove_original 
-		remove_original(youtube.filename, args['dir'], video_format)
-	
+    # Delete original video file if -do/--delete and -c/--convert is given
+    if args['c'] and args['do']:
+    
+    # Call remove_original 
+        remove_original(youtube.filename, args['dir'], video_format)
+    
 
 def check_for_mp3_codec(filename):
-	"""
-	Uses a subprocess call to ffprobe 
-	to get audio codec output
-	on flv videos.
+    """
+    Uses a subprocess call to ffprobe 
+    to get audio codec output
+    on flv videos.
 
-	filename	:	filename of video to check codecs
+    filename    :   filename of video to check codecs
 
-	"""
+    """
 
-	# Set subprocess to be called
-	ffprobe_cmd		=		'ffprobe -show_streams'.split()
+    # Set subprocess to be called
+    ffprobe_cmd     =       'ffprobe -show_streams'.split()
 
-	# Append filename to subprocess cmd
-	ffprobe_cmd.append(filename)
+    # Append filename to subprocess cmd
+    ffprobe_cmd.append(filename)
 
-	# Call and return the output of the subprocess
-	return subprocess.check_output(ffprobe_cmd)
+    # Call and return the output of the subprocess
+    return subprocess.check_output(ffprobe_cmd)
 
 
 def video_to_mp3(filename):
-	"""
-	Uses a subprocess call to FFMPEG 
-	for converting the video file to mp3 format
+    """
+    Uses a subprocess call to FFMPEG 
+    for converting the video file to mp3 format
 
-	filename	:	filename of video to be converted
+    filename    :   filename of video to be converted
 
-	"""
+    """
 
-	# Split beginning of ffmpeg_cmd for subprocess calling and append filename
-	ffmpeg_cmd		=		'ffmpeg -i'.split()
-	ffmpeg_cmd.append(filename)
+    # Split beginning of ffmpeg_cmd for subprocess calling and append filename
+    ffmpeg_cmd      =       'ffmpeg -i'.split()
+    ffmpeg_cmd.append(filename)
 
-	# Checking for mp4 format
-	if 'mp4' in filename:
+    # Checking for mp4 format
+    if 'mp4' in filename:
 
-	# Replace original extension with mp3
-		ffmpeg_cmd.append(filename.replace('.mp4', '.mp3'))
+    # Replace original extension with mp3
+        ffmpeg_cmd.append(filename.replace('.mp4', '.mp3'))
 
-	# Check for flv format
-	elif 'flv' in filename:
+    # Check for flv format
+    elif 'flv' in filename:
 
-	# Check if flv video already has an mp3 audio codec
-		try:
-	
-	# Use a regular expression to search output of ffprobe for mp3 codec
-			if 'mp3' in re.findall(r'(codec_name=mp3)', check_for_mp3_codec(filename))[0]:
+    # Check if flv video already has an mp3 audio codec
+        try:
+    
+    # Use a regular expression to search output of ffprobe for mp3 codec
+            if 'mp3' in re.findall(r'(codec_name=mp3)', check_for_mp3_codec(filename))[0]:
 
-	# Extend ffmpeg list with conversion instructions
-				ffmpeg_cmd.extend('-acodec copy -ac 2 -ab 128k -vn -n'.split())
+    # Extend ffmpeg list with conversion instructions
+                ffmpeg_cmd.extend('-acodec copy -ac 2 -ab 128k -vn -n'.split())
 
-	# Append output filename with .mp3 extension
-				ffmpeg_cmd.append(filename.replace('.flv', '.mp3'))
-	
-	# If mp3 isn't found in ffprobe's output use regular conversion options
-		except IndexError:
-			ffmpeg_cmd.append(filename.replace('.flv', '.mp3'))
+    # Append output filename with .mp3 extension
+                ffmpeg_cmd.append(filename.replace('.flv', '.mp3'))
+    
+    # If mp3 isn't found in ffprobe's output use regular conversion options
+        except IndexError:
+            ffmpeg_cmd.append(filename.replace('.flv', '.mp3'))
 
-	# Check for 3gp format
-	elif '3gp' in filename:
-		ffmpeg_cmd.append(filename.replace('.3gp', '.mp3'))
+    # Check for 3gp format
+    elif '3gp' in filename:
+        ffmpeg_cmd.append(filename.replace('.3gp', '.mp3'))
 
-	else:
-	# Otherwise should be .webm format
-		ffmpeg_cmd.append(filename.replace('.webm', '.mp3'))
+    else:
+    # Otherwise should be .webm format
+        ffmpeg_cmd.append(filename.replace('.webm', '.mp3'))
 
-	# Call ffmpeg subprocess
-	try:
-		subprocess.call(ffmpeg_cmd)
+    # Call ffmpeg subprocess
+    try:
+        subprocess.call(ffmpeg_cmd)
 
-	# Catch non-existant file and if ffmpeg is not installed
-	except OSError:
-		print '\n\nError: Check whether video file exists or that ffmpeg is installed.\n\n'
-		exit(1)
+    # Catch non-existant file and if ffmpeg is not installed
+    except OSError:
+        print('\n\nError: Check whether video file exists or that ffmpeg is installed.\n\n')
+        exit(1)
 
 
 def remove_original(filename, arg_dir, form):
-	"""
-	Removes original video file if both -c/--convert
-	and -do/--delete options are given.
+    """
+    Removes original video file if both -c/--convert
+    and -do/--delete options are given.
 
-	filename	:	filename of video to be deleted
+    filename    :   filename of video to be deleted
 
-	arg_dir		:	directory containing video file
+    arg_dir     :   directory containing video file
 
-	form		:	format extension of video file
+    form        :   format extension of video file
 
-	"""
+    """
 
-	# Join the directory and filename with extension
-	pathname		=	abspath(path_join(arg_dir, '{0}.{1}'.format(filename, form)))
+    # Join the directory and filename with extension
+    pathname        =   abspath(path_join(arg_dir, '{0}.{1}'.format(filename, form)))
 
-	# Remove original video
-	remove(pathname)
+    # Remove original video
+    remove(pathname)
